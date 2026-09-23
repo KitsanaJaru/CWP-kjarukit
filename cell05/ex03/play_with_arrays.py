e2b8e42c570e4b3818799d5c46a6a7c21f9f85a8
KitsanaJaru <kitsanajaru@gmail.com>
2026-09-24 03:10:12 +0700

    Create play_with_arrays.py

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,11 @@
+nums = [2, 8, 9, 48, 8, 22, -12, 2]
+print(nums)
+
+for i in range(len(nums) - 1, -1, -1):
+    nums[i] += 2
+    if nums[i] < 5:
+        del nums[i]
+
+nums = list(set(nums))
+
+print(nums)
