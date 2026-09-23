36737880b263e3e62e7d2bbf1cf3ad9bc94d0178
KitsanaJaru <kitsanajaru@gmail.com>
2026-09-23 22:17:15 +0700

    Create age.py

---
+++ cell04/ex01/age.py
@@ -0,0 +1,9 @@
+age = int(input("Please tell me your age: "))
+
+print(f"You are currently {age} years old.")
+
+x = 10
+
+while x <= 30:
+    print(f"In {x} years, you will be {age + x} years old.")
+    x += 10
