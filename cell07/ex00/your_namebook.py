00495f4a997b091858c93317305ff3c368b68f28
KitsanaJaru <kitsanajaru@gmail.com>
2026-09-25 00:36:20 +0700

    rush00/ex00

---
+++ cell07/ex00/your_namebook.py
@@ -0,0 +1,11 @@
+def array_of_names(names):
+    return list(names)
+
+persons = {
+    "jean": "valjean",
+    "grace": "hopper",
+    "xavier": "niel",
+    "fifi": "brindacier"
+}
+
+print(array_of_names(persons))
