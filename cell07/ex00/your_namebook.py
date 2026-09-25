0d04137b8eccb1d5c4a7ac927ed7f51b8613615e
KitsanaJaru <kitsanajaru@gmail.com>
2026-09-25 12:47:50 +0700

    Update your_namebook.py

--- cell07/ex00/your_namebook.py
+++ cell07/ex00/your_namebook.py
@@ -1,5 +1,12 @@
 def array_of_names(names):
-    return list(names)
+    result = []
+
+    for first_name, last_name in names.items():
+        full_name = first_name.capitalize() + " " + last_name.capitalize()
+        result.append(full_name)
+
+    return result
+
 
 persons = {
     "jean": "valjean",
